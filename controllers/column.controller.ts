871bca04ce3cb991bdb2f393ceb5d94598669f17
johnneon <neon.1598@mail.ru>
2021-01-22 21:20:21 +0600

    fix: fix put columns

--- controllers/column.controller.ts
+++ controllers/column.controller.ts
@@ -1,5 +1,5 @@
 import Column, { IColumn } from './../models/Column';
-import Board from './../models/Board';
+import Board, { IBoard } from './../models/Board';
 import Card from '../models/Card';
 
 const CreateColumn = async ({ name, position, boardId }) => {
@@ -30,10 +30,30 @@ const UpdateColumn = async ({ id, name, position }) => {
     }
     
     if (position || position === 0) {
+      const columns = await Column.find({ boardId: column.boardId });
+      columns.sort((a, b) => a.position - b.position);
+
+      const currentPos = column.position;
+      const desiredPos = position;
+      const operation = columns.splice(currentPos, 1);
+      columns.splice(desiredPos, 0, ...operation);
+      columns.forEach((el, ind) => el.position = ind );
+    
+      const bulkArr = [];
+    
+      for (const input of columns) {
+        bulkArr.push({
+          updateOne: {
+            filter: { _id: input._id },
+            update: { position: input.position }
+          }
+        });
+      }
+    
+      await Column.bulkWrite(bulkArr);
+
       column.position = position;
     }
-
-    column.save();
 
     return column;
   }
