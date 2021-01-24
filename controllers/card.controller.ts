dcd6f9de890055a00c115660c381d424c3f0b724
johnneon <neon.1598@mail.ru>
2021-01-24 21:40:53 +0600

    fix: fix change card positions

--- controllers/card.controller.ts
+++ controllers/card.controller.ts
@@ -1,93 +1,160 @@
-import Column, { IColumn } from './../models/Column';
-import Board from './../models/Board';
+import { Request, Response } from 'express';
+import Column from './../models/Column';
 import Card, { ICard } from '../models/Card';
+import global from '../variables';
 
-const CreateCard = async ({ name, position, columnId }) => {
-  return await Card
-    .create({ name, position, columnId })
-    .then( async (card: ICard) => {
-      console.log(card._id)
-      await Column.updateOne( { _id: columnId }, { $push: { cards: { _id: card._id } } });
-      return card;
-    })
-    .catch((err) => err);
+const CreateCard = async (req: Request, res: Response) => {
+  try {
+    const { name, columnId } = req.body;
+
+    const check = global.FORBIDDEN_SYMBOLS_REGEXP;
+  
+    if (check.test(name)) {
+      return res.status(400).json({ message: global.INCORECT_CHARTS });
+    }
+  
+    if (!columnId) {
+      return res.status(400).json({ message: global.COLUMN_NOT_FOUND });
+    }
+
+    const cards = await Card.find({ columnId });
+
+    let cardPosition: number = 0;
+
+    if (cards.length) {
+      cardPosition = cards.length;
+    }
+  
+    const card = await Card
+      .create({ name, position: cardPosition, columnId })
+      .then( async (card: ICard) => {
+        console.log(card._id)
+        await Column.updateOne( { _id: columnId }, { $push: { cards: { _id: card._id } } });
+        return card;
+      })
+      .catch((err) => err);
+
+    return res.status(201).json(card);
+  } catch (e) {
+    return res.status(500).json({ message: global.RANDOM_ERROR });
+  }
 }
 
-const GetCard = async ({ id }) => {
-  const card: ICard = await Card.findById(id);
+const GetCard = async (req: Request, res: Response) => {
+  try {
+    const card: ICard = await Card.findById(req.params.id);
 
-  if (card) {
-    return card;
+    if (!card) {
+      return res.status(404).json({ message: global.CARD_NOT_FOUND });
+    }
+
+    return res.status(201).json(card);
+  } catch (e) {
+    return res.status(500).json({ message: global.RANDOM_ERROR });
   }
 };
 
-const UpdateCard = async ({ name, position, content, columnId, id }) => {
-    const card: ICard = await Card.findById(id);
+const UpdateCard = async (req: Request, res: Response) => {
+  try {
+    const { name, position, content, columnId } = req.body;
 
-    if (name) {
-      card.name = name;
+    const card: ICard = await Card.findById(req.params.id);
+
+    if (!card) {
+      return res.status(404).json({ message: global.CARD_NOT_FOUND });
     }
 
-    if (columnId && position) {
-      const prevColumn = card.columnId;
+    const chengeName = () => {
+      const check = global.FORBIDDEN_SYMBOLS_REGEXP;
+  
+      if (check.test(name)) {
+        return res.status(400).json({ message: global.INCORECT_CHARTS });
+      }
+
+      card.name = name;
+    };
+
+    const sortCards = async (cards: Array<ICard>) => {
+      cards.forEach((el, ind) => el.position = ind );
+      console.log(cards);
+    
+      const bulkArr = [];
+    
+      for (const input of cards) {
+        bulkArr.push({
+          updateOne: {
+            filter: { _id: input._id },
+            update: { position: input.position }
+          }
+        });
+      }
+
+      await Card.bulkWrite(bulkArr).catch((e) => e);
+    };
+
+    const changeCardsPositions = async (cards: Array<ICard>) => {
+      cards.sort((a, b) => a.position - b.position);
+      cards.forEach((el, ind) => el.position = ind );
+
+      const currentPos = card.position;
+      const desiredPos = position;
+      const operation = cards.splice(currentPos, 1);
+      cards.splice(desiredPos, 0, ...operation);
+      
+      
+      await sortCards(cards);
+    }
+
+    const changePosition = async () => {
+      const cards = await Card.find({ columnId: card.columnId });
+
+      if (position > (cards.length - 1) || position < 0) {
+        return res.status(400).json({ message: 'Incorect position!' });
+      }
+    
+      await changeCardsPositions(cards);
+
+      card.position = position;
+    }
+
+    const changeColumn = async () => {
+      const column = await Column.findById(card.columnId);
+      const prevColumn = column._id;
+      const currentColumn = columnId;
+
+      if (!column) {
+        return res.status(400).json({ message: global.COLUMN_NOT_FOUND });
+      }
       
       await Column.updateOne(
         { _id: prevColumn },
-        { $pull: { cards: id } }
-      );
+        { $pull: { cards: req.params.id } }
+      ).catch((e) => e);
 
       await Column.updateOne(
-        { _id: columnId },
-        { $push: { cards: id } }
+        { _id: currentColumn },
+        { $push: { cards: req.params.id } }
       );
 
-      const cards = await Card.find({ boardId: card.columnId });
-      cards.sort((a, b) => a.position - b.position);
+      await Card.updateOne({ _id: req.params.id }, { columnId: currentColumn }).catch((e) => e);
 
-      const currentPos = card.position;
-      const desiredPos = position;
-      const operation = cards.splice(currentPos, 1);
-      cards.splice(desiredPos, 0, ...operation);
-      cards.forEach((el, ind) => el.position = ind );
-    
-      const bulkArr = [];
-    
-      for (const input of cards) {
-        bulkArr.push({
-          updateOne: {
-            filter: { _id: input._id },
-            update: { position: input.position }
-          }
-        });
-      }
-    
-      await Card.bulkWrite(bulkArr);
+      const prevColumnCards = await Card.find({ columnId: prevColumn });
 
-      card.columnId = columnId;
+      await sortCards(prevColumnCards);
+
+      const currentColumnCards = await Card.find({ columnId: currentColumn });
+
+      await changeCardsPositions(currentColumnCards);
+    }
+
+    if (name) {
+      chengeName();
+    }
+
+    if (columnId) {
+      await changeColumn();
     } else if (position || position === 0) {
-      const cards = await Card.find({ boardId: card.columnId });
-      cards.sort((a, b) => a.position - b.position);
-
-      const currentPos = card.position;
-      const desiredPos = position;
-      const operation = cards.splice(currentPos, 1);
-      cards.splice(desiredPos, 0, ...operation);
-      cards.forEach((el, ind) => el.position = ind );
-    
-      const bulkArr = [];
-    
-      for (const input of cards) {
-        bulkArr.push({
-          updateOne: {
-            filter: { _id: input._id },
-            update: { position: input.position }
-          }
-        });
-      }
-    
-      await Card.bulkWrite(bulkArr);
-
-      card.position = position;
+      await changePosition();
     }
 
     if (content) {
@@ -96,19 +163,32 @@ const UpdateCard = async ({ name, position, content, columnId, id }) => {
 
     card.save();
 
-    return card;
+    return res.status(201).json(card);
+  } catch (e) {
+    return res.status(500).json({ message: global.RANDOM_ERROR });
+  }
 };
 
-const DeleteCard = async ({ id }) => {
-  const card: ICard = await Card.findByIdAndDelete(id);
+const DeleteCard = async (req: Request, res: Response) => {
+  try {
+    const card: ICard = await Card.findByIdAndDelete(req.params.id);
 
-  if (card) {
-    await Column.updateOne(
-      { _id: card.columnId },
-      { $pull: { cards: id } }
-    );
-    
-    return card;
+    if (card) {
+      await Column.updateOne(
+        { _id: card.columnId },
+        { $pull: { cards: req.params.id } }
+      );
+      
+      return card;
+    }
+
+    if (!card) {
+      return res.status(404).json({ message: global.CARD_NOT_FOUND });
+    }
+
+    return res.status(200).json({ message: global.CARD_DELETED });
+  } catch (e) {
+    return res.status(500).json({ message: global.RANDOM_ERROR });
   }
 };
 
--- routes/cards.ts
+++ routes/cards.ts
@@ -1,83 +1,15 @@
-import { Request, Response, Router } from 'express';
-import { ICard } from '../models/Card';
+import { Router } from 'express';
 import auth from '../middleware/auth.middleware';
-import global from '../variables';
 import CardController from '../controllers/card.controller';
 
 const cardsRouter = Router();
 
-cardsRouter.post('/', auth, async (req: Request, res: Response) => {
-  try {
-    const { name, columnId } = req.body;
+cardsRouter.post('/', auth, CardController.CreateCard);
 
-    const check = global.FORBIDDEN_SYMBOLS_REGEXP;
+cardsRouter.get('/:id', auth, CardController.GetCard);
 
-    if (check.test(name)) {
-      return res.status(400).json({ message: global.INCORECT_CHARTS });
-    }
+cardsRouter.put('/:id', auth, CardController.UpdateCard);
 
-    if (!columnId) {
-      return res.status(400).json({ message: global.COLUMN_NOT_FOUND });
-    }
-
-    const card: Promise<ICard> = await CardController.CreateCard({ ...req.body });
-
-    return res.status(201).json(card);
-  } catch (e) {
-    return res.status(500).json({ message: global.RANDOM_ERROR });
-  }
-});
-
-cardsRouter.get('/:id', auth, async (req: Request, res: Response) => {
-  try {
-    const card: ICard = await CardController.GetCard({ id: req.params.id });
-
-    if (!card) {
-      return res.status(404).json({ message: global.CARD_NOT_FOUND });
-    }
-
-    return res.status(201).json(card);
-  } catch (e) {
-    return res.status(500).json({ message: global.RANDOM_ERROR });
-  }
-});
-
-cardsRouter.put('/:id', auth, async (req: Request, res: Response) => {
-  try {
-    const { name } = req.body;
-
-    if (name) {
-      const check = global.FORBIDDEN_SYMBOLS_REGEXP;
-  
-      if (check.test(name)) {
-        return res.status(400).json({ message: global.INCORECT_CHARTS });
-      }
-    }
-
-    const card: ICard = await CardController.UpdateCard({ id: req.params.id, ...req.body });
-
-    if (!card) {
-      return res.status(404).json({ message: global.CARD_NOT_FOUND });
-    }
-
-    return res.status(201).json(card);
-  } catch (e) {
-    return res.status(500).json({ message: global.RANDOM_ERROR });
-  }
-});
-
-cardsRouter.delete('/:id', auth, async (req: Request, res: Response) => {
-  try {
-    const card: ICard = await CardController.DeleteCard({ id: req.params.id });
-
-    if (!card) {
-      return res.status(404).json({ message: global.CARD_NOT_FOUND });
-    }
-
-    return res.status(200).json({ message: global.CARD_DELETED });
-  } catch (e) {
-    return res.status(500).json({ message: global.RANDOM_ERROR });
-  }
-});
+cardsRouter.delete('/:id', auth, CardController.DeleteCard);
 
 export default cardsRouter;
