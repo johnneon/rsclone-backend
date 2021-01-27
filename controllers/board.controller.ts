e0458b8c23b1e58cc42dfa4578c335352512866e
johnneon <neon.1598@mail.ru>
2021-01-27 18:18:35 +0600

    refactor: refactor board controller

--- controllers/board.controller.ts
+++ controllers/board.controller.ts
@@ -1,89 +1,137 @@
+import { Errback, Request, Response } from 'express';
 import Board, { IBoard } from './../models/Board';
 import User, { IUser } from '../models/User';
+import global from '../variables';
 
-const CreateBoard = async ({ name, user }) => {
-  return await Board
-    .create({ name, users: [user.userId] })
-    .then( async (data: IBoard) => {
-      await User.updateOne({ _id: user.userId }, {'$push' : {'boards': data._id} });
-      return data;
-    })
-    .catch((err) => err);
+const CreateBoard = async (req: Request, res: Response) => {
+  try {
+    const { name, user } = req.body;
+  
+    const check = global.FORBIDDEN_SYMBOLS_REGEXP;
+  
+    if (check.test(name)) {
+      return res.status(400).json({ message: global.INCORECT_CHARTS });
+    }
+  
+    const board: IBoard =  await Board
+      .create({ name, users: [user.userId] })
+      .then( async (data: IBoard) => {
+        await User.updateOne({ _id: user.userId }, {'$push' : {'boards': data._id} });
+        return data;
+      })
+      .catch((err) => err);
+  
+    return res.status(201).json(board);
+  } catch (e) {
+    return res.status(500).json({ message: global.RANDOM_ERROR });
+  }
 }
 
-const GetFullBoard = async ({ id }) => {
-  const boardPopulate = {
-    path: 'columns',
-    populate: {
-      path: 'cards'
+const GetFullBoard = async (req: Request, res: Response) => {
+  try {
+    const boardPopulate = {
+      path: 'columns',
+      populate: {
+        path: 'cards'
+      }
     }
-  }
+  
+    const board: IBoard = await Board
+      .findById(req.params.id)
+      .populate(boardPopulate)
+      .populate('users');
 
-  const board: IBoard = await Board
-    .findById(id)
-    .populate(boardPopulate)
-    .populate('users');
-
-  if (board) {
+    if (!board) {
+      return res.status(404).json({ message: global.BOARD_NOT_FOUND }); 
+    }
+  
     const { users } = board;
-
+  
     users.map((user: IUser) => {
       user.password = undefined;
       user.boards = undefined;
     });
-    return board;
+
+    return res.status(201).json(board);
+  } catch (e) {
+    return res.status(500).json({ message: global.RANDOM_ERROR });
+  }
+  
+};
+
+const GetAllBoards = async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.body.user;
+
+    const boards: any = await Board.find({ users: { $in: [ userId ] } });
+
+    boards.map((board) => {
+      board.users = undefined;
+      board.columns = undefined;
+    });
+
+    return res.status(201).json(boards);
+  } catch (e) {
+    return res.status(500).json({ message: global.RANDOM_ERROR });
   }
 };
 
-const GetAllBoards = async ({ userId }) => {
-  const boards: any = await Board.find({ users: { $in: [ userId ] } });
+const UpdateBoard = async (req: Request, res: Response) => {
+  try {
+    const { name } = req.body;
+    const _id = req.params.id;
 
-  boards.map((board) => {
-    board.users = undefined;
-    board.columns = undefined;
-  });
+    const board: IBoard = await Board.findOneAndUpdate({ _id }, { name });
+    if (!board) {
+      return res.status(404).json({ message: global.BOARD_NOT_FOUND }); 
+    }
 
-  return boards;
-};
-
-const UpdateBoard = async ({ name, _id}) => {
-  const board: IBoard = await Board.findOneAndUpdate({ _id }, { name });
-
-  if (board) {
     const updatedBoard: IBoard = await Board.findById({ _id });
       
     updatedBoard.users = undefined;
     updatedBoard.columns = undefined;
 
-    return updatedBoard;
+    return res.status(201).json(updatedBoard);
+  } catch (e) {
+    return res.status(500).json({ message: global.RANDOM_ERROR });
   }
 };
 
-const DeleteBoard = async ({ _id, userId }) => {
-  const board: IBoard = await Board.findOne({ _id }, async (err, Board) => {
-    if (err) {
-      throw err;
+const DeleteBoard = async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.body.user;
+    const _id = req.params.id;
+    
+    const board: IBoard = await Board.findOne({ _id }, async (err: Errback, Board: IBoard) => {
+      if (err) {
+        throw err;
+      }
+  
+      const { users } = Board;
+  
+      const user = users.indexOf(userId);
+  
+      if (user !== -1) {
+        users.splice(user, 1);
+      }
+  
+      if (Board.users.length === 0) {
+        Board.delete();
+      }
+    });
+
+    if (!board) {
+      return res.status(404).json({ message: global.BOARD_NOT_FOUND }); 
     }
-
-    const { users } = Board;
-
-    const user = users.indexOf(userId);
-
-    if (user !== -1) {
-      users.splice(user, 1);
-    }
-
-    if (Board.users.length === 0) {
-      Board.delete();
-    }
-  });
-
-  if (board) {
+  
     await User.updateOne(
       { _id: userId },
       { $pull: { boards: _id } }
     );
-    return board;
+
+    return res.status(201).json({ message: global.LEFT_BOARD });
+  } catch (e) {
+    return res.status(500).json({ message: global.RANDOM_ERROR });
   }
 };
 
--- routes/boards.ts
+++ routes/boards.ts
@@ -6,76 +6,14 @@ import BoardController from '../controllers/board.controller';
 
 const boardRouter = Router();
 
-boardRouter.post('/', auth, async (req: Request, res: Response) => {
-  try {
-    const { name } = req.body;
+boardRouter.post('/', auth, BoardController.CreateBoard);
 
-    const check = global.FORBIDDEN_SYMBOLS_REGEXP;
+boardRouter.get('/:id', auth, BoardController.GetFullBoard);
 
-    if (check.test(name)) {
-      return res.status(400).json({ message: global.INCORECT_CHARTS });
-    }
+boardRouter.get('/', auth, BoardController.GetAllBoards);
 
-    const board: Promise<IBoard> = await BoardController.CreateBoard({ ...req.body });
+boardRouter.put('/:id', auth, BoardController.UpdateBoard);
 
-    return res.status(201).json(board);
-  } catch (e) {
-    return res.status(500).json({ message: global.RANDOM_ERROR });
-  }
-});
-
-boardRouter.get('/:id', auth, async (req: Request, res: Response) => {
-  try {
-    const board: IBoard = await BoardController.GetFullBoard({ id: req.params.id });
-
-    if (!board) {
-      return res.status(404).json({ message: global.BOARD_NOT_FOUND }); 
-    }
-
-    return res.status(201).json(board);
-  } catch (e) {
-    return res.status(500).json({ message: global.RANDOM_ERROR });
-  }
-});
-
-boardRouter.get('/', auth, async (req: Request, res: Response) => {
-  try {
-    const boards: any = await BoardController.GetAllBoards({ ...req.body.user });
-
-    return res.status(201).json(boards);
-  } catch (e) {
-    return res.status(500).json({ message: global.RANDOM_ERROR });
-  }
-});
-
-boardRouter.put('/:id', auth, async (req: Request, res: Response) => {
-  try {
-    const board: IBoard = await BoardController.UpdateBoard({ _id: req.params.id, ...req.body  });
-
-    if (!board) {
-      return res.status(404).json({ message: global.BOARD_NOT_FOUND }); 
-    }
-
-    return res.status(201).json(board);
-  } catch (e) {
-    return res.status(500).json({ message: global.RANDOM_ERROR });
-  }
-});
-
-boardRouter.delete('/:id', auth, async (req: Request, res: Response) => {
-  try {
-    const { userId } = req.body.user;
-    
-    const board: IBoard = await BoardController.DeleteBoard({ _id: req.params.id, userId  });
-
-    if (!board) {
-      return res.status(404).json({ message: global.BOARD_NOT_FOUND }); 
-    }
-
-    return res.status(201).json({ message: global.LEFT_BOARD });
-  } catch (e) {
-    return res.status(500).json({ message: global.RANDOM_ERROR });
-  }
-});
+boardRouter.delete('/:id', auth, BoardController.DeleteBoard);
 
 export default boardRouter;
