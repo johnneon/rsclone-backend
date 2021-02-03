b18df2407089446015e4bc48583060aeda300217
johnneon <neon.1598@mail.ru>
2021-02-03 20:05:15 +0600

    refactor: refactor board controller

--- controllers/board.controller.ts
+++ controllers/board.controller.ts
@@ -182,7 +182,6 @@ const InviteUser = async (req: Request, res: Response) => {
         return res.status(400).json({ message: USER_INVITED });
       }
     }
-    
 
     await User.findOneAndUpdate({ email: to }, {
       $push: { notifications: { from, to, boardId, boardName: board.name } }
